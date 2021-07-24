ea1a1aa8954ac094fd223deb0461f6dfc6187314
melkdesousa <melk.sousap@gmail.com>
2021-07-24 12:32:02 -0300

    chore: environment variables

---
+++ src/config/environment.ts
@@ -0,0 +1,9 @@
+export const MYSQL_DATABASE = process.env.MYSQL_DATABASE
+
+export const MYSQL_USER = process.env.MYSQL_USER
+
+export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
+
+export const MYSQL_PORT = process.env.MYSQL_DOCKER_PORT
+
+export const PORT = process.env.PORT
