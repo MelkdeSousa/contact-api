9795d3b84bc3fec8a7e28841846f9c4a77cb9f09
melkdesousa <melk.sousap@gmail.com>
2021-08-04 18:58:17 -0300

    server: versioning routes

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,15 @@ import express from 'express'
 import cors from 'cors'
 
 import { PORT } from './config/environment'
-import routes from './routes'
+import routesV1 from './routes/v1'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/v1/', routes)
+
+app.use('/api/v1/', routesV1)
+
+app.get('/', (_, res) => res.json({ message: 'welcome to Contact API' }))
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
